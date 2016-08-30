b14cdbf892aa82fbf086245cf39873d88d618578
SK207_inst <SK207_inst@K207_Inst.carrollcc.edu>
2016-08-29 21:30:48 -0400

    Solution

--- LabOne/src/edu/cis232/UserChecker.java
+++ LabOne/src/edu/cis232/UserChecker.java
@@ -11,6 +11,22 @@ public class UserChecker {
 	public static void main(String[] args) throws IOException {
 		ArrayList<String> users = loadUsers();
 		//printUsers(users);
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Enter a username");
+		String input = keyboard.nextLine();
+		
+		boolean found = false;
+		for(String user:users){
+			if(user.equalsIgnoreCase(input)){
+				System.out.println("Hello " +user);
+				found = true;
+			}
+		}
+		
+		if(!found){
+			System.out.println("Access Denied");
+		}
+		
 	}
 
 	private static void printUsers(ArrayList<String> users) {
